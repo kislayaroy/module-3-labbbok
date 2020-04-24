147c470af162998ce9e50ba2ddab04c01c21a959
kislayaroy <61449659+kislayaroy@users.noreply.github.com>
2020-04-24 20:26:42 +0530

    Add files via upload

---
+++ lab 1/Lab1.3/emp/src/main/java/com/cg/Employee.java
@@ -0,0 +1,65 @@
+package com.cg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+public class Employee {
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int employeeId, String employeeName, double salary, int age){
+		this.employeeId=employeeId;
+		this.employeeName=employeeName;
+		this.salary=salary;
+		this.age=age;
+	}
+	
+	private int employeeId;
+	
+	private String employeeName;
+	
+	private double salary;
+	
+	private int age;
+
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void printDetails() {
+		System.out.println("Employee Details");
+		System.out.println("--------");
+		System.out.println("Employee Id : "+employeeId);
+		System.out.println("Employee Name : "+employeeName);
+		System.out.println("Employee Salary : "+salary);
+		System.out.println("Employee Age : "+age);
+		
+	}
+	
+}
